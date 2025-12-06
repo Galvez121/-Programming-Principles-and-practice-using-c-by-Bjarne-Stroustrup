d58dcea9efe3fff34fe9b57cc1f3eb212687f16b
Juan Galvez <55637706+Galvez121@users.noreply.github.com>
2025-12-06 20:35:41 +0000

    Part2: Add program to compare two numbers and display smaller and larger values
    - Implemented logic to determine and print the smaller and larger values from user input.
    - Added a loop to allow repeated comparisons until the user decides to stop.
    - Improved user prompts for clarity.

---
+++ Section 3 computation/Drills/part2.cpp
@@ -0,0 +1,37 @@
+//  Change the program to write out  
+// the smaller value is: followed by the smaller of the numbers  
+// the larger value is: followed by the larger value.
+// Learning c++ 
+
+#include <iostream>
+
+int main() {
+  int val1;
+  int val2;
+
+  std::string stop;
+
+  while(stop !=  "|"){
+
+    std::cout << "Enter two numerical vales: \n";
+    std::cin >> val1 >> val2;
+
+   // Logic to determine and print smaller and larger values
+   if(val1 > val2){
+      std::cout << "the smaller  value is: " << val2;
+    }
+    else if(val1 < val2){
+      std::cout << "the larger value is: "  << val2;
+    }
+    else{
+      std::cout << "Values are the same";
+    }
+
+    std::cout << " \n Again? Enter | for not: \n ";
+    std::cin >> stop;
+   
+  }
+ 
+
+  std::cout << "Done";
+}
